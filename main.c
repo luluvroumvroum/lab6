210ad76de27390a0b1686ef9793701ad3192a8ec
lucas <lucas.gallelli@efrei.net>
2025-03-28 10:40:53 +0100

    maon

---
+++ main.c
@@ -0,0 +1,10 @@
+//
+// Created by bruno on 28/03/2025.
+//
+
+#include "main.h"
+#include <stdio.h>
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by bruno on 28/03/2025.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#endif //MAIN_H
